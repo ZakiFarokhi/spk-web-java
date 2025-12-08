88f55767ba0f2941c2dd1794b1200d66ce8f3e47
admin <admin@admins-MacBook-Pro.local>
2025-12-08 23:40:22 +0700

    add sql

---
+++ src/main/resources/sql/data-sub-criteria.sql
@@ -0,0 +1,50 @@
+INSERT INTO sub_criteria (code, criteria_id, name) VALUES
+-- C1 PENDIDIKAN
+('C1.1', 1, 'SMA'),
+('C1.2', 1, 'D3'),
+('C1.3', 1, 'S1/DIV'),
+('C1.4', 1, 'S2'),
+('C1.5', 1, 'S3'),
+
+-- C2 HASIL KERJA
+('C2.1', 2, 'ST'),
+('C2.2', 2, 'Jumlah LHP Terbit Tepat'),
+('C2.3', 2, 'Kesesuaian LHP dengan Standar Audit'),
+('C2.4', 2, 'Kertas Kerja Pengawasan (SKP)'),
+
+-- C3 PERILAKU KERJA
+('C3.1', 3, 'LHP Revisi/Koreksi'),
+('C3.2', 3, 'Jumlah LHP Belum Terbit'),
+('C3.3', 3, 'Tidak Masuk Kerja Tanpa Keterangan'),
+('C3.4', 3, 'Terlambat Jam Kerja'),
+('C3.5', 3, 'Pulang Kerja Lebih Awal'),
+('C3.6', 3, 'Hukuman Disiplin'),
+
+-- C4 KOMPETENSI
+('C4.1', 4, 'Diklat/Sertifikasi'),
+('C4.2', 4, 'Ketua Tim/Kasubtim'),
+('C4.3', 4, 'Pelayanan Konsultasi'),
+('C4.4', 4, 'Penguasan Teknik Audit'),
+('C4.5', 4, 'Penugasan Khusus/Koordinator'),
+('C4.6', 4, 'Kerjasama Tim'),
+
+-- C5 SKP
+('C5.1', 5, 'Sangat Baik'),
+('C5.2', 5, 'Baik'),
+('C5.3', 5, 'Cukup'),
+('C5.4', 5, 'Kurang'),
+('C5.5', 5, 'Sangat Kurang'),
+
+-- C6 ANGKA KREDIT
+('C6.1', 6, 'Sangat Baik'),
+('C6.2', 6, 'Baik'),
+('C6.3', 6, 'Cukup'),
+('C6.4', 6, 'Kurang'),
+('C6.5', 6, 'Sangat Kurang'),
+
+-- C7 PENILAIAN ATASAN
+('C7.1', 7, 'Kinerja Terbaik'),
+('C7.2', 7, 'Baik'),
+('C7.3', 7, 'Kinerja Cukup'),
+('C7.4', 7, 'Kinerja Kurang'),
+('C7.5', 7, 'Kinerja Sangat Kurang');
